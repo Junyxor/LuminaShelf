65d6a6d607c067d4e4302465396201a46742f9e5
Kiana Kaslana <130233273+Junyxor@users.noreply.github.com>
2026-09-10 17:46:34 +0800

    style: load account workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./network-settings.css";
+import "./account.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
