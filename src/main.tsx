a7445628bac9d04ad6a6e2731fe1f162c0a7ca4a
Kiana Kaslana <130233273+Junyxor@users.noreply.github.com>
2026-09-13 13:44:42 +0800

    style: load download task controls

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./network-settings.css";
 import "./account.css";
 import "./android-native.css";
+import "./downloads.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
